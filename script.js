b2eeed562ffaeb6378e05e7c7d22b1804447ff92
Connor Neill <cwmn1975@comcast.net>
2025-08-14 15:31:52 -0400

    Create operate function
    Created operate function which takes two numbers and an operator and then calls a math operation function.

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ function multiply (a, b) {
 function divide (a, b) {
     return a / b;
 }
+
+function operate (a, b, operator) {
+    switch (operator) {
+        case '+':
+            add(a, b);
+            break;
+        case '-':
+            subtract(a, b);
+            break;
+        case '*':
+            multiply(a, b);
+            break;
+        case '/':
+            divide(a, b);
+    }
+}
